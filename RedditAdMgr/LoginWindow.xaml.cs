ce05c1afbd53b000ff312c01acc8ca64471f52fa
Jhon Delgado <jhadelgadoca@unal.edu.co>
2017-02-23 03:38:39 -0500

    - Improved threaded log in - Implemented user settings for log in and main form

--- RedditAdMgr/LoginWindow.xaml.cs
+++ RedditAdMgr/LoginWindow.xaml.cs
@@ -3,7 +3,9 @@ using System;
 using System.IO;
 using System.Net;
 using System.Text;
+using System.Threading;
 using System.Windows;
+using System.Threading.Tasks;
 
 namespace RedditAdMgr
 {
@@ -17,16 +19,61 @@ namespace RedditAdMgr
         public LoginWindow()
         {
             InitializeComponent();
+            LoginProgressLabel.Visibility = Visibility.Hidden;
+            LoginProgressBar.Visibility = Visibility.Hidden;
+            UserNameTextBox.Text = Properties.Settings.Default.Username;
+            PasswordTextBox.Password = Properties.Settings.Default.Password;
         }
 
         private void SubmitButton_Click(object sender, RoutedEventArgs e)
         {
+            if (string.IsNullOrEmpty(UserNameTextBox.Text) || string.IsNullOrEmpty(PasswordTextBox.Password))
+            {
+                MessageBox.Show("Username or password fields are empty!");
+            }
+            else
+            {
+                var taskScheduler = TaskScheduler.FromCurrentSynchronizationContext();
+                var ct = new CancellationToken();
+                string c = string.Empty;
+                string username = UserNameTextBox.Text, password = PasswordTextBox.Password;
+
+                SubmitButton.IsEnabled = false;
+                LoginProgressBar.Visibility = Visibility.Visible;
+                LoginProgressBar.IsIndeterminate = true;
+                LoginProgressLabel.Visibility = Visibility.Visible;
+                LoginProgressLabel.Content = "Logging into Reddit...";
+
+                Task.Factory.StartNew(() => Login(username, password, out c)).
+                    ContinueWith(w =>
+                   {
+                       SubmitButton.IsEnabled = true;
+                       LoginProgressBar.Visibility = Visibility.Hidden;
+                       LoginProgressLabel.Visibility = Visibility.Hidden;
+
+                       if (c.Contains("reddit_session"))
+                       {
+                           MessageBox.Show("Logged in!", "Success");
+
+                           if (RememberPasswordCheckBox.IsChecked == true)
+                           {
+                               Properties.Settings.Default.Username = username;
+                               Properties.Settings.Default.Password = password;
+                               Properties.Settings.Default.Save();
+                           }
+                       }
+                       else
+                           MessageBox.Show("Incorrect credentials, please try again!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                   },
+                   ct,
+                   TaskContinuationOptions.None, taskScheduler);
+            }
         }
 
-        private void Login()
+        private void Login(string username, string password, out string cookie)
         {
             string loginUrl = "https://www.reddit.com/api/login/";
-            string loginParams = string.Format("op=login&user={0}&passwd={1}&api_type=json", UserNameTextBox.Text, PasswordTextBox.Password);
+            string loginParams = string.Format("op=login&user={0}&passwd={1}&api_type=json", username, password);
 
             //statusLbl.Visibility = Visibility.Visible;
             //progressBar.Visibility = Visibility.Visible;
@@ -47,13 +94,7 @@ namespace RedditAdMgr
             }
 
             HttpWebResponse response = loginRequest.GetResponse() as HttpWebResponse;
-            string cookie = response.Headers["set-cookie"];
-
-            //statusLbl.Content = "Logged in!";
-
-            cookies = new CookieContainer();
-
-            cookies.SetCookies(new Uri("http://reddit.com"), cookie);
+            cookie = response.Headers["set-cookie"];
         }
     }
 }
